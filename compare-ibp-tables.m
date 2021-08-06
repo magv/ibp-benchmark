663277065ac3546bd34cfea2088663daf79a5cc2
Vitaly Magerya <magv@tx97.net>
2021-08-06 09:25:34 +0200

    Don't stop at the first IBP table comparison fail

--- compare-ibp-tables.m
+++ compare-ibp-tables.m
@@ -13,7 +13,7 @@ table1 = Get[filename1] /. B[1,idx__] :> basis[idx] // Association;
 masters1 = table1 // Values // Cases[#, _basis, {0, Infinity}]& // Union;
 Print["Table 1 has ", masters1 // Length, " masters, ", table1 // Length, " rules"];
 
-Print["Table 2: ", filename2];
+Print["Table 2 is ", filename2];
 table2 = Get[filename2] /. B[1,idx__] :> basis[idx];
 masters2 = table2 // Values // Cases[#, _basis, {0, Infinity}]& // Union;
 Print["Table 2 has ", masters2 // Length, " masters, ", table2 // Length, " rules"];
@@ -38,6 +38,20 @@ ProbablyZeroQ[ex_] := Module[{vars, map},
     {Power::infy, Infinity::indet}]
 ]
 
+Print["* Checking integrals"];
+
+Print["Table 1 has ", Complement[masters1, Keys[table2], masters2]//Length, " masters not in table 2"];
+Complement[masters1, Keys[table2], masters2] // MapIndexed[Print[#2//First, ") ", #1]&];
+
+Print["Table 2 has ", Complement[masters2, Keys[table1], masters1]//Length, " masters not in table 1"];
+Complement[masters2, Keys[table1], masters1] // MapIndexed[Print[#2//First, ") ", #1]&];
+
+If[Sort[masters1] === Sort[masters2],
+  Print["The master sets are the same!"],
+  Print["The master sets are different!"]
+];
+
+nerrors = 0;
 integrals = Join[table1 // Keys, table2 // Keys, masters1, masters2] // Union;
 Do[
   integral = integrals[[idx]];
@@ -45,19 +59,21 @@ Do[
   ex1 = integral /. table1;
   ex2 = integral /. table2 /. table1;
   If[Not[MemberQ[table1 // Keys, integral] || MemberQ[masters1, integral]],
-    Print["Integral ", integral, " is missing from table 1"];
-    Exit[1];
+    Print["! Integral ", integral, " is missing from table 1"];
+    nerrors += 1;
+    Continue[];
   ];
   If[Not[MemberQ[table2 // Keys, integral] || MemberQ[masters2, integral]],
-    Print["Integral ", integral, " is missing from table 2"];
-    Exit[1];
+    Print["! Integral ", integral, " is missing from table 2"];
+    nerrors += 1;
+    Continue[];
   ];
   diff = Collect[ex1 - ex2, _basis, If[ProbablyZeroQ[#],0,Coeff[#]]&];
   If[diff =!= 0,
     Print["Mismatch in ", integral];
-    Print["Difference between table1 and table2:"];
-    Print[diff];
-    Exit[1];
+    nerrors += 1;
+    Continue[];
   ];
   ,
   {idx, Length[integrals]}]
+Print["Errors: ", nerrors];
