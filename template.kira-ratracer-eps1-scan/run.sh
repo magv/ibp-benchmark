9343b49b192e916b424a08f157df877784508ba3
Vitaly Magerya <magv@tx97.net>
2022-10-27 18:18:46 +0200

    Update the templates to support preferred_masters

--- template.kira-ratracer-eps1-scan/run.sh
+++ template.kira-ratracer-eps1-scan/run.sh
@@ -8,10 +8,15 @@ rm -rf ff_save/ firefly_saves/ results/ sectormappings/ tmp/ kira.log
 
 /usr/bin/time ratracer \
     set d '4-2*eps' \
+#% if p.preferred_masters:
+    define-family master \
+    load-equations master-eqns \
+#% pass
     $(for x in input_kira/basis/*.kira.gz; do echo load-equations $x; done) \
     solve-equations \
     choose-equation-outputs --family=basis --maxr={p.maxr} --maxs={p.maxs} --maxd={p.maxd} \
     drop-equations \
+    keep-outputs output-list \
     optimize finalize save-trace uds.trace.zst \
     to-series eps 1 \
     finalize unfinalize optimize finalize \
@@ -22,4 +27,12 @@ rm -rf ff_save/ firefly_saves/ results/ sectormappings/ tmp/ kira.log
     measure \
     reconstruct --inmem --factor-scan --shift-scan --threads={p.threads} --to=ibp-tables
 
+#% if p.preferred_masters:
+/usr/bin/time sed -E ibp-tables \
+#%     for i, indices in enumerate(p.preferred_masters):
+        -e 's/\bmaster\@{i}\b/basis[{",".join(str(i) for i in indices)}]/g' \
+#%     pass
+    | ./rat2mat >ibp-tables.m
+#% else:
 /usr/bin/time ./rat2mat <ibp-tables >ibp-tables.m
+#% pass
--- template.kira-ratracer/run.sh
+++ template.kira-ratracer/run.sh
@@ -7,10 +7,15 @@ rm -rf input_kira/ ff_save/ firefly_saves/ results/ sectormappings/ tmp/ kira.lo
 rm -rf ff_save/ firefly_saves/ results/ sectormappings/ tmp/ kira.log
 
 /usr/bin/time ratracer \
+#% if p.preferred_masters:
+    define-family master \
+    load-equations master-eqns \
+#% pass
     $(for x in input_kira/basis/*.kira.gz; do echo load-equations $x; done) \
     solve-equations \
     choose-equation-outputs --family=basis --maxr={p.maxr} --maxs={p.maxs} --maxd={p.maxd} \
     drop-equations \
+    keep-outputs output-list \
     optimize finalize save-trace uds.trace.zst
 
 /usr/bin/time ratracer \
@@ -18,4 +23,12 @@ rm -rf ff_save/ firefly_saves/ results/ sectormappings/ tmp/ kira.log
     measure \
     reconstruct --inmem --threads={p.threads} --to=ibp-tables
 
+#% if p.preferred_masters:
+/usr/bin/time sed -E ibp-tables \
+#%     for i, indices in enumerate(p.preferred_masters):
+        -e 's/\bmaster\@{i}\b/basis[{",".join(str(i) for i in indices)}]/g' \
+#%     pass
+    | ./rat2mat >ibp-tables.m
+#% else:
 /usr/bin/time ./rat2mat <ibp-tables >ibp-tables.m
+#% pass
